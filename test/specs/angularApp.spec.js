6f5e23e07c0fadcd7355365ea7f251a2770bdeb1
Valeryia Strom <vstrom@its.jnj.com>
2020-04-25 21:55:17 +0300

    fix mistakes

--- test/specs/angularApp.spec.js
+++ test/specs/angularApp.spec.js
@@ -1,11 +1,16 @@
 describe('test for Angular app', function() {
   const URL = 'https://www.samsung-forward.ru';
 
-  it('catalog should contain 12 product items', function() {
-    browser.manage().timeouts().implicitlyWait(5000)
-    browser.get(URL);
-    element(by.css('.page-header a.menu__item:first-child')).click();
+  it('catalog should contain 11 product items', async function() {
+    browser.waitForAngularEnabled(false);
+    await browser.get(URL);
+    let EC = protractor.ExpectedConditions;
+    let catalogButton = element(by.css('.page-header a.menu__item:first-child'));
+    await browser.wait(EC.elementToBeClickable(catalogButton), 10000);
+    await catalogButton.click();
+    let lastCatalogItem = element.all(by.css('div.product-card__image')).get(10);
+    await browser.wait(EC.visibilityOf(lastCatalogItem), 10000);
     let catalogItems = element.all(by.css('div.product-card__image'));
-    expect(catalogItems.count()).toBe(12);
+    expect(catalogItems.count()).toBe(11);
   });
 });
