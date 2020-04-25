2d2f171cc82cbaad59ad4a93f3d9755ddad3d26a
Valeryia Strom <vstrom@its.jnj.com>
2020-04-25 20:58:20 +0300

    fix mistakes related to asynchronous actions

--- test/specs/nonAngularApp.spec.js
+++ test/specs/nonAngularApp.spec.js
@@ -2,21 +2,21 @@ describe('test for non-Angular app', function() {
   const URL = 'https://www.indiegogo.com/'
   const searchQuery = 'beauty';
 
-  it('should have at least one beauty topic campaign', function() {
+  it('should have at least one beauty topic campaign', async function() {
     browser.manage().timeouts().implicitlyWait(5000)
     browser.waitForAngularEnabled(false);
-    browser.get(URL);
-    element(by.css('.findItFirst-actions button:last-child')).click();
+    await browser.get(URL);
+    await element(by.css('.findItFirst-actions button:last-child')).click();
     let EC = protractor.ExpectedConditions;
     let exploreProductsButton = element(by.css('.exploreProjects-button .i-cta-1'));
-    browser.wait(EC.visibilityOf(exploreProductsButton), 4000 ,'button was not found');
-    exploreProductsButton.click();
+    await browser.wait(EC.visibilityOf(exploreProductsButton), 4000 ,'button was not found');
+    await exploreProductsButton.click();
     let searchField = element(by.css('.search-desktop .i-search-bar-form input'));
-    browser.wait(EC.visibilityOf(searchField), 4000, 'search field was not found');
-    searchField.click().sendKeys(searchQuery);
-    browser.actions().sendKeys(protractor.Key.ENTER);
+    await browser.wait(EC.visibilityOf(searchField), 4000, 'search field was not found');
+    await searchField.click().sendKeys(searchQuery);
+    await browser.actions().sendKeys(protractor.Key.ENTER);
     let campaignTitle = element(by.xpath('(//*[contains(@class,"discoverableCard-title")])[1]'));
-    browser.wait(EC.visibilityOf(campaignTitle), 4000, 'no campaings was found');
+    await browser.wait(EC.visibilityOf(campaignTitle), 4000, 'no campaings was found');
     expect(campaignTitle.isPresent()).toBe(true);
   });
 });
